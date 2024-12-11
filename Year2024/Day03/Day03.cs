cc5bd1dd08680038ac762c390fc323a99c57ff18
Krzysztof Gornowicz <krzysztofgornowicz@gmail.com>
2024-12-11 22:19:20 +0100

    Rewritten Day03 of 2024

--- Year2024/Day03/Day03.cs
+++ Year2024/Day03/Day03.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AdventOfCode.Year2024
@@ -18,16 +19,34 @@ namespace AdventOfCode.Year2024
         private void ParseInput(string inputFilePath)
         {
             var lines = StaticHelpers.GetLines(inputFilePath);
+            line = string.Join("", lines);
         }
+
+        string line;
 
         public void Task1()
         {
-            
+            Console.WriteLine(CalculateResult(line));
         }
 
         public void Task2()
         {
+            Regex dontdoPattern = new Regex("don't\\(\\)(.*?)do\\(\\)");
+            line = dontdoPattern.Replace(line, string.Empty);
 
+            Console.WriteLine(CalculateResult(line));
+        }
+
+        private long CalculateResult(string input)
+        {
+            long result = 0;
+            Regex mulpattern = new Regex("mul\\((\\d+,\\d+)\\)");
+            foreach (Match match in mulpattern.Matches(input))
+            {
+                var splitMatch = match.Groups[1].Value.Split(',');
+                result += long.Parse(splitMatch[0]) * long.Parse(splitMatch[1]);
+            }
+            return result;
         }
     }
 }
