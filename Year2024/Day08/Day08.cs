40a61417e7c9699b3e546663b7183fba48fe7bd9
Krzysztof Gornowicz <krzysztofgornowicz@gmail.com>
2024-12-13 00:17:46 +0100

    Rewritten Day08 of 2024

--- Year2024/Day08/Day08.cs
+++ Year2024/Day08/Day08.cs
@@ -10,6 +10,10 @@ namespace AdventOfCode.Year2024
 {
     class Day08 : IDay
     {
+        int maxH, maxW;
+        bool[,] visited;
+        List<(char freq, int i, int j)> antennas = new List<(char freq, int i, int j)>();
+
         public Day08(string inputFilePath)
         {
             ParseInput(inputFilePath);
@@ -18,16 +22,74 @@ namespace AdventOfCode.Year2024
         private void ParseInput(string inputFilePath)
         {
             var lines = StaticHelpers.GetLines(inputFilePath);
+            maxH = lines.Length;
+            maxW = lines[0].Length;
+            visited = new bool[maxH, maxW];
+            for (int i = 0; i < maxH; i++)
+            {
+                for (int j = 0; j < maxW; j++)
+                {
+                    if (lines[i][j] != '.')
+                    {
+                        antennas.Add((lines[i][j], i, j));
+                    }
+                }
+            }
         }
 
         public void Task1()
         {
-            
+            foreach (var antenna in antennas)
+            {
+                foreach (var connectedAntenna in antennas.Where(a => a.freq == antenna.freq && (a.i != antenna.i || a.j != antenna.j)))
+                {
+                    int difi = antenna.i - connectedAntenna.i;
+                    int difj = antenna.j - connectedAntenna.j;
+                    if (antenna.i + difi >= 0 && antenna.i + difi < maxH && antenna.j + difj >= 0 && antenna.j + difj < maxW)
+                    {
+                        visited[antenna.i + difi, antenna.j + difj] = true;
+                    }
+                }
+            }
+            var result = 0;
+            for(int i =0; i < maxH; i++)
+            {
+                for(int j = 0;j < maxW; j++)
+                {
+                    result += visited[i, j] ? 1 : 0;
+                }
+            }
+            Console.WriteLine(result);
         }
 
         public void Task2()
         {
-
+            foreach (var antenna in antennas)
+            {
+                foreach (var connectedAntenna in antennas.Where(a => a.freq == antenna.freq && (a.i != antenna.i || a.j != antenna.j)))
+                {
+                    int difi = antenna.i - connectedAntenna.i;
+                    int difj = antenna.j - connectedAntenna.j;
+                    int currenti = antenna.i;
+                    int currentj = antenna.j;
+                    visited[currenti, currentj] = true;
+                    while (currenti + difi >= 0 && currenti + difi < maxH && currentj + difj >= 0 && currentj + difj < maxW)
+                    {
+                        visited[currenti + difi, currentj + difj] = true;
+                        currenti += difi;
+                        currentj += difj;
+                    }
+                }
+            }
+            var result = 0;
+            for (int i = 0; i < maxH; i++)
+            {
+                for (int j = 0; j < maxW; j++)
+                {
+                    result += visited[i, j] ? 1 : 0;
+                }
+            }
+            Console.WriteLine(result);
         }
     }
 }
