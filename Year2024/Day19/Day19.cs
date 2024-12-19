607f2ed5db8b2f6802f8bc7d10a8f895ff3486ac
Krzysztof Gornowicz <krzysztofgornowicz@gmail.com>
2024-12-19 14:26:39 +0100

    Day19 of 2024

--- Year2024/Day19/Day19.cs
+++ Year2024/Day19/Day19.cs
@@ -10,6 +10,11 @@ namespace AdventOfCode.Year2024
 {
     class Day19 : IDay
     {
+        List<string> patterns = new List<string>();
+        List<string> designs = new List<string>();
+        Dictionary<string, long> testedDesigns = new Dictionary<string, long>();
+
+
         public Day19(string inputFilePath)
         {
             ParseInput(inputFilePath);
@@ -18,16 +23,65 @@ namespace AdventOfCode.Year2024
         private void ParseInput(string inputFilePath)
         {
             var lines = StaticHelpers.GetLines(inputFilePath);
+            var splitPatterns = lines[0].Split(", ");
+            foreach (var splitPattern in splitPatterns)
+            {
+                patterns.Add(splitPattern);
+            }
+            for (int i = 2; i != lines.Length; i++)
+            {
+                designs.Add(lines[i]);
+            }
         }
 
         public void Task1()
         {
-            
+            long result = 0;
+            var impossibleDesigns = new List<string>();
+            foreach (var design in designs)
+            {
+                long possibleAlignments = CheckIfDesignPossible(design, true);
+                if (possibleAlignments == 0)
+                {
+                    impossibleDesigns.Add(design);
+                }
+                result += possibleAlignments;
+            }
+            designs = designs.Except(impossibleDesigns).ToList();
+            Console.WriteLine(result);
         }
 
         public void Task2()
         {
+            long result = 0;
+            foreach (var design in designs)
+            {
+                result += CheckIfDesignPossible(design, false);
+            }
+            Console.WriteLine(result);
+        }
 
+        long CheckIfDesignPossible(string design, bool isPartOne)
+        {
+            if(testedDesigns.TryGetValue(design, out long previousResult))
+            {
+                return previousResult;
+            }
+            long result = 0;
+            foreach (var pattern in patterns)
+            {
+                if (isPartOne && result > 0) return 1;
+                if (design == pattern)
+                {
+                    result++;
+                }
+                else if (design.StartsWith(pattern))
+                {
+                    result += CheckIfDesignPossible(design.Substring(pattern.Length), isPartOne);
+                }
+            }
+            testedDesigns.Add(design, result);
+            return result;
         }
     }
 }
