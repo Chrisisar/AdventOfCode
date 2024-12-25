ee260e38126a377924937f347deaa12d229c657f
Krzysztof Gornowicz <krzysztofgornowicz@gmail.com>
2024-12-25 10:54:25 +0100

    Day25 of 2024

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace AdventOfCode
         static void Main(string[] args)
         {
             //NewYearGenerator.GenerateNextYear(2024);
-            Type dayType = typeof(Day23);
+            Type dayType = typeof(Day25);
 
 
             var files = StaticHelpers.GetAllTxtFiles(dayType);
--- Year2024/Day25/Day25.cs
+++ Year2024/Day25/Day25.cs
@@ -10,6 +10,9 @@ namespace AdventOfCode.Year2024
 {
     class Day25 : IDay
     {
+        List<int[]> locks;
+        List<int[]> keys;
+
         public Day25(string inputFilePath)
         {
             ParseInput(inputFilePath);
@@ -17,12 +20,53 @@ namespace AdventOfCode.Year2024
 
         private void ParseInput(string inputFilePath)
         {
+            locks = new List<int[]>();
+            keys = new List<int[]>();
             var lines = StaticHelpers.GetLines(inputFilePath);
+            for (int i = 0; i < lines.Length; i += 8)
+            {
+                bool isLock = lines[i].Equals("#####");
+                int[] newKeyLock = new int[5];
+                for (int j = i + 1; j < i + 6; j++)
+                {
+                    for(int k =0;k <5;k++)
+                    {
+                        if (lines[j][k] == '#')
+                        {
+                            newKeyLock[k]++;
+                        }
+                    }
+                }
+                if(isLock)
+                {
+                    locks.Add(newKeyLock);
+                }
+                else
+                {
+                    keys.Add(newKeyLock);
+                }
+
+            }
         }
 
         public void Task1()
         {
-            
+            var result = 0;
+            for(int i =0; i < keys.Count; i++)
+            {
+                for(int j =0;j<locks.Count;j++)
+                {
+                    for(int k =0;k<5;k++)
+                    {
+                        if (keys[i][k] + locks[j][k] > 5) break;
+                        if(k==4)
+                        {
+                            result++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(result);
         }
 
         public void Task2()
