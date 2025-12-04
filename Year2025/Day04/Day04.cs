b3bbb05fd7d8c85fea3acacfcc9a278284e92d32
Krzysztof Gornowicz <krzysztofgornowicz@gmail.com>
2025-12-04 22:47:01 +0100

    Day 4 of 2025

--- Year2025/Day04/Day04.cs
+++ Year2025/Day04/Day04.cs
@@ -1,15 +1,13 @@
 using AdventOfCode.Helpers;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventOfCode.Year2025
 {
     class Day04 : IDay
     {
+        bool[,] wall;
+        int Height, Width;
+
         public Day04(string inputFilePath)
         {
             ParseInput(inputFilePath);
@@ -18,16 +16,99 @@ namespace AdventOfCode.Year2025
         private void ParseInput(string inputFilePath)
         {
             var lines = StaticHelpers.GetLines(inputFilePath);
+            Height = lines.Length + 2;
+            Width = lines[0].Length +2;
+            wall = new bool[Height, Width];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for(int j = 0; j < lines[i].Length; j++)
+                {
+                    wall[i + 1, j + 1] = lines[i][j] == '@';
+                }
+            }
         }
 
         public void Task1()
         {
-            
+            var result = 0;
+            for(int i=0; i < Height; i++)
+            {
+                for(int j=0; j < Width; j++)
+                {
+                    if (wall[i,j])
+                    {
+                        if(CountSurroundingPaperStacks(i, j) <4)
+                        {
+                            result++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(result);
+        }
+
+        private int CountSurroundingPaperStacks(int i, int j)
+        {
+            var result = 0;
+            if (wall[i - 1, j - 1])
+            {
+                result++;
+            }
+            if (wall[i - 1, j])
+            {
+                result++;
+            }
+            if (wall[i - 1, j + 1])
+            {
+                result++;
+            }
+            if (wall[i, j - 1])
+            {
+                result++;
+            }
+            if (wall[i, j + 1])
+            {
+                result++;
+            }
+            if (wall[i + 1, j - 1])
+            {
+                result++;
+            }
+            if (wall[i + 1, j])
+            {
+                result++;
+            }
+            if (wall[i + 1, j + 1])
+            {
+                result++;
+            }
+            return result;
         }
 
         public void Task2()
         {
-
+            var result = 0;
+            var shouldContinue = true;
+            while (shouldContinue)
+            {
+                shouldContinue = false;
+                for (int i = 0; i < Height; i++)
+                {
+                    for (int j = 0; j < Width; j++)
+                    {
+                        if (wall[i, j])
+                        {
+                            if (CountSurroundingPaperStacks(i, j) < 4)
+                            {
+                                shouldContinue = true;
+                                wall[i, j] = false;
+                                result++;
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(result);
         }
     }
 }
